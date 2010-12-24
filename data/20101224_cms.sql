5a88534ef46c39dd8a7298b545d1620d9556db64
Cole Hafner <colehafner@gmail.com>
2010-12-24 10:38:04 -0800

    new data schema

---
+++ data/20101224_cms.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 24, 2010 at 10:36 AM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `common_Users`
+--
+
+DROP TABLE IF EXISTS `common_Users`;
+CREATE TABLE `common_Users` (
+  `user_id` int(100) NOT NULL AUTO_INCREMENT,
+  `thumb_id` int(100) DEFAULT '0',
+  `user_type_id` int(100) DEFAULT '0',
+  `username` varchar(1000) NOT NULL,
+  `email` varchar(1000) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `first_name` varchar(1000) DEFAULT NULL,
+  `last_name` varchar(1000) DEFAULT NULL,
+  `bio` varchar(5000) DEFAULT NULL,
+  `use_gravatar` tinyint(1) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  KEY `thumb_id` (`thumb_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `common_Users`
+--
+
